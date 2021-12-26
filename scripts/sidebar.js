3e8ee34c706b1bbe4f8a253e82b44bbac024cd58
Willian Cavalcanti Coelho <minegt1500@gmail.com>
2021-12-26 08:27:47 -0300

    metadata created

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -3,8 +3,9 @@ const mobileMenu = document.querySelector('.mobile-menu')
 const iconList = document.querySelector('#icon-img')
 mobileMenu.addEventListener('click', () => {
     if (navList.style.display == '') {
+        const languageSelector = document.querySelector('.language-selector')
         navList.style.display = 'grid'
-        iconList.innerHTML = `<a href=''>Login</a>`
+        iconList.innerHTML = `<a href='/${languageSelector.value}/login/login.html'>Login</a>`
         iconList.color = 'white'
         console.log(iconList.innerHTML)
     } else {
